7cd7bb5dc65343d14d57cd155bcbbc3dbecea600
Zhenya Hitrova <jarotea@yandex.ru>
2024-06-24 17:51:10 +1000

    Routes try 2

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,17 @@
-import { ConstructorPage } from '@pages';
+import { ConstructorPage, Feed } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+    </Routes>
   </div>
 );
 
