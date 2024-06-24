e42bb2767c647f81c7ce441726fdb7bda34e20c5
Zhenya Hitrova <jarotea@yandex.ru>
2024-06-25 01:06:19 +1000

    Nav links work

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -3,33 +3,45 @@ import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
 import {
   BurgerIcon,
+  Button,
   ListIcon,
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
-  <header className={styles.header}>
-    <nav className={`${styles.menu} p-4`}>
-      <div className={styles.menu_part_left}>
-        <>
-          <BurgerIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </>
-        <>
-          <ListIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </>
-      </div>
-      <div className={styles.logo}>
-        <Logo className='' />
-      </div>
-      <div className={styles.link_position_last}>
-        <ProfileIcon type={'primary'} />
-        <p className='text text_type_main-default ml-2'>
-          {userName || 'Личный кабинет'}
-        </p>
-      </div>
-    </nav>
-  </header>
-);
+export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
+  const navigate = useNavigate();
+  const navigateTo = (path: string) => navigate(path);
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.menu} p-4`}>
+        <div className={styles.menu_part_left}>
+          <div onClick={() => navigateTo('/')} className={styles.link}>
+            <BurgerIcon type={'primary'} />
+            <p className='text text_type_main-default ml-2 mr-10'>
+              Конструктор
+            </p>
+          </div>
+          <div onClick={() => navigateTo('/feed')} className={styles.link}>
+            <ListIcon type={'primary'} />
+            <p className='text text_type_main-default ml-2'>Лента заказов</p>
+          </div>
+        </div>
+        <div onClick={() => navigateTo('/')} className={styles.logo}>
+          <Logo className='' />
+        </div>
+        <div
+          onClick={() => navigateTo('/profile')}
+          className={styles.link_position_last}
+        >
+          <ProfileIcon type={'primary'} />
+          <p className='text text_type_main-default ml-2'>
+            {userName || 'Личный кабинет'}
+          </p>
+        </div>
+      </nav>
+    </header>
+  );
+};
