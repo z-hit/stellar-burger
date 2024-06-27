74a6b9868aa751fbfb09b9c2fefad1859eee8dc4
Zhenya Hitrova <jarotea@yandex.ru>
2024-06-27 21:29:18 +1000

    Nav links work

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -9,38 +9,61 @@ import {
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const navigate = useNavigate();
   const navigateTo = (path: string) => navigate(path);
 
+  const location = useLocation();
+  const isActive = (path: string) => path === location.pathname;
+  const linkClasses = clsx('link', { link_active: isActive });
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
-          <div onClick={() => navigateTo('/')} className={styles.link}>
+          <NavLink
+            to={'/'}
+            className={({ isActive }) =>
+              clsx(styles.link, {
+                [styles.link_active]: isActive
+              })
+            }
+          >
             <BurgerIcon type={'primary'} />
             <p className='text text_type_main-default ml-2 mr-10'>
               Конструктор
             </p>
-          </div>
-          <div onClick={() => navigateTo('/feed')} className={styles.link}>
+          </NavLink>
+          <NavLink
+            to={'/feed'}
+            className={({ isActive }) =>
+              clsx(styles.link, {
+                [styles.link_active]: isActive
+              })
+            }
+          >
             <ListIcon type={'primary'} />
             <p className='text text_type_main-default ml-2'>Лента заказов</p>
-          </div>
+          </NavLink>
         </div>
-        <div onClick={() => navigateTo('/')} className={styles.logo}>
+        <NavLink to={'/'} className={styles.logo}>
           <Logo className='' />
-        </div>
-        <div
-          onClick={() => navigateTo('/profile')}
-          className={styles.link_position_last}
+        </NavLink>
+        <NavLink
+          to={'/profile'}
+          className={({ isActive }) =>
+            clsx(styles.link_position_last, {
+              [styles.link_active]: isActive
+            })
+          }
         >
           <ProfileIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>
             {userName || 'Личный кабинет'}
           </p>
-        </div>
+        </NavLink>
       </nav>
     </header>
   );
