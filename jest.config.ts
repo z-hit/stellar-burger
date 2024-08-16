abb345755598430baa51806f92e1e2f6898d1573
Zhenya Hitrova <jarotea@yandex.ru>
2024-08-16 15:51:34 +1000

    verbose problem fixed

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,27 @@ import { compilerOptions } from './tsconfig.json';
 
 const jestConfig: JestConfigWithTsJest = {
   verbose: true,
+  bail: false,
+  roots: ['<rootDir>'],
+  testEnvironment: 'node',
   transform: {
     preset: 'ts-jest',
     '^.+\\.tsx?$': ['ts-jest', {}]
   },
+  testPathIgnorePatterns: [
+    '<rootDir>/build/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/'
-  })
+  }),
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[tj]s?(x)'
+  ]
+  //testRegex: '.*/src/.*\\.test\\.(t|j)sx?$'
 };
 
 export default jestConfig;
