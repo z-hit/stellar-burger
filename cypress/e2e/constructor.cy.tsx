52f97a366ecd10c7a691a42cf7da553e11dd054e
Zhenya Hitrova <jarotea@yandex.ru>
2024-08-17 18:53:29 +1000

    ingredients test doesn't work

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,5 +1,7 @@
+/// <reference types="cypress" />
+
 describe('test Constructor ppage', () => {
-  test('test getIngredients API returns ingredients', () => {
+  it('test getIngredients API returns ingredients', () => {
     cy.intercept('GET', `${URL}/ingredients`, {
       fixture: 'ingredients.json'
     }).as('getIngredients');
@@ -8,6 +10,18 @@ describe('test Constructor ppage', () => {
 
     cy.wait('@getIngredients');
 
-    cy.get('h1').should('contain', 'Dashboard');
+    cy.get('[data-testid="ingredients"]').should('contain', {
+      _id: '111',
+      name: 'Space bun',
+      type: 'bun',
+      proteins: 11,
+      fat: 21,
+      carbohydrates: 31,
+      calories: 111,
+      price: 100,
+      image: 'image-url',
+      image_large: 'some-url',
+      image_mobile: 'some-url2'
+    });
   });
 });
--- src/components/ui/burger-ingredients/burger-ingredients.tsx
+++ src/components/ui/burger-ingredients/burger-ingredients.tsx
@@ -42,7 +42,7 @@ export const BurgerIngredientsUI: FC<BurgerIngredientsUIProps> = memo(
             </Tab>
           </ul>
         </nav>
-        <div className={styles.content}>
+        <div className={styles.content} data-testid='ingredients'>
           <IngredientsCategory
             title='Булки'
             titleRef={titleBunRef}
