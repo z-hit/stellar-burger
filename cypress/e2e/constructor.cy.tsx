61097f5404b602f6e5d71eec6b914d8ce73ac9cf
Zhenya Hitrova <jarotea@yandex.ru>
2024-08-18 23:23:26 +1000

    Cy test order burger process ready

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -2,6 +2,14 @@
 
 describe('test Constructor page', () => {
   const testUrl = 'http://localhost:4000/';
+  const modal = `[data-cy='modal']`;
+  const ingredientsSection = `[data-cy='ingredients']`;
+  const constructorSection = `[data-cy='constructor']`;
+  const bunTop = `[data-cy='bun-top']`;
+  const bunBottom = `[data-cy='bun-bottom']`;
+  const noBun = `[data-cy='no-bun']`;
+  const main = `[data-cy='main']`;
+  const ingredient = `[data-cy='ingredient']`;
 
   beforeEach(() => {
     cy.intercept('GET', `api/ingredients`, {
@@ -18,43 +26,62 @@ describe('test Constructor page', () => {
   });
 
   it('test getIngredients API returns ingredients', () => {
-    cy.visit('http://localhost:4000/');
+    cy.visit(testUrl);
 
-    cy.get(`[data-cy='ingredients']`).should('contain', 'Space bun');
-    cy.get(`[data-cy='ingredients']`).should('contain', 'Space meat');
-    cy.get(`[data-cy='ingredients']`).should('contain', 'Space sauce');
+    cy.get(ingredientsSection).should('contain', 'Space bun');
+    cy.get(ingredientsSection).should('contain', 'Space meat');
+    cy.get(ingredientsSection).should('contain', 'Space sauce');
   });
 
   it('test add ingredients to constructor', () => {
-    cy.visit('http://localhost:4000/');
+    cy.visit(testUrl);
 
-    cy.get(`[data-cy='ingredients']`).find('button').click({ multiple: true });
-    cy.get(`[data-cy='bun']`).should('contain', 'Space bun');
-    cy.get(`[data-cy='main']`).should('contain', 'Space meat');
-    cy.get(`[data-cy='main']`).should('contain', 'Space sauce');
+    cy.get(ingredientsSection).find('button').click({ multiple: true });
+    cy.get(bunTop).should('contain', 'Space bun');
+    cy.get(bunBottom).should('contain', 'Space bun');
+    cy.get(main).should('contain', 'Space meat');
+    cy.get(main).should('contain', 'Space sauce');
   });
 
   it('test open modal and close with "close" button', () => {
-    cy.visit('http://localhost:4000/');
+    cy.visit(testUrl);
 
-    cy.get(`[data-cy='ingredient']`).first().click();
-    cy.get(`[data-cy='modal']`).should('exist');
-    cy.get(`[data-cy='modal']`).find('button').click();
-    cy.get(`[data-cy='modal']`).should('not.exist');
+    cy.get(ingredient).first().click();
+    cy.get(modal).should('exist');
+    cy.get(modal).find('button').click();
+    cy.get(modal).should('not.exist');
   });
 
   it('test open modal and close with "overlay" click', () => {
-    cy.visit('http://localhost:4000/');
+    cy.visit(testUrl);
 
-    cy.get(`[data-cy='ingredient']`).first().click();
-    cy.get(`[data-cy='modal']`).should('exist');
+    cy.get(ingredient).first().click();
+    cy.get(modal).should('exist');
     cy.get('body').click('topLeft');
-    cy.get(`[data-cy='modal']`).should('not.exist');
+    cy.get(modal).should('not.exist');
   });
 
   it('test user is authed', () => {
     cy.setCookie('accessToken', 'someToken');
     cy.visit(testUrl);
     cy.get(`[data-cy='user-name']`).should('contain.text', 'cat42');
+  });
+
+  it('test burger constructed, ordered, Modal opens, order number correct, modal closes, constructor clears', () => {
+    cy.setCookie('accessToken', 'someToken');
+    cy.visit(testUrl);
+
+    cy.get(ingredientsSection).find('button').click({ multiple: true });
+    cy.get(bunTop).should('contain', 'Space bun');
+    cy.get(bunBottom).should('contain', 'Space bun');
+    cy.get(main).should('contain', 'Space meat');
+    cy.get(main).should('contain', 'Space sauce');
+    cy.contains('Оформить заказ').click();
+    cy.get(modal).should('exist');
+    cy.get(modal).find('h2').should('contain.text', '98765');
+    cy.get(modal).find('button').click();
+    cy.get(modal).should('not.exist');
+    cy.get(main).should('contain.text', 'Выберите начинку');
+    cy.get(noBun).should('contain.text', 'Выберите булки');
   });
 });
--- src/components/ui/burger-constructor/burger-constructor.tsx
+++ src/components/ui/burger-constructor/burger-constructor.tsx
@@ -20,7 +20,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
 }) => (
   <section className={styles.burger_constructor} data-cy='constructor'>
     {constructorItems.bun ? (
-      <div className={`${styles.element} mb-4 mr-4`} data-cy='bun'>
+      <div className={`${styles.element} mb-4 mr-4`} data-cy='bun-top'>
         <ConstructorElement
           type='top'
           isLocked
@@ -31,6 +31,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
       </div>
     ) : (
       <div
+        data-cy='no-bun'
         className={`${styles.noBuns} ${styles.noBunsTop} ml-8 mb-4 mr-5 text text_type_main-default`}
       >
         Выберите булки
@@ -57,7 +58,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
       )}
     </ul>
     {constructorItems.bun ? (
-      <div className={`${styles.element} mt-4 mr-4`} data-cy='bun'>
+      <div className={`${styles.element} mt-4 mr-4`} data-cy='bun-bottom'>
         <ConstructorElement
           type='bottom'
           isLocked
@@ -68,6 +69,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
       </div>
     ) : (
       <div
+        data-cy='no-bun'
         className={`${styles.noBuns} ${styles.noBunsBottom} ml-8 mb-4 mr-5 text text_type_main-default`}
       >
         Выберите булки
